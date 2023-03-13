063322ec26d53a3d7eb67ecef2c5c45b9d9ab353
Mohammed Imran <mohammedimran86992@gmail.com>
2023-03-13 21:59:58 +0530

    removed @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mohammedimran.com',
+    experimental: {
+        assets: true,
+    },
     integrations: [
         tailwind(),
         sitemap({
             changefreq: 'always',
             lastmod: new Date(),
         }),
-        image(),
     ],
 });
