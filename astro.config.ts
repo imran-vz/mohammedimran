3455a16e829f124e9302fb4569e7335d54dc8e22
Mohammed Imran <mohammedimran86992@gmail.com>
2023-11-19 18:01:38 +0530

    Add Astro Svelte plugin to svelte.config.js and package.json

--- astro.config.ts
+++ astro.config.ts
@@ -2,18 +2,24 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-// import node from "@astrojs/node";
+import node from "@astrojs/node";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    // output: "hybrid",
-    // adapter: node({
-    //     mode: "standalone",
-    // }),
+    output: "hybrid",
+    adapter: node({
+        mode: "standalone",
+    }),
     site: "https://mohammedimran.com",
     integrations: [
         tailwind(),
-        sitemap({ changefreq: "always", lastmod: new Date() }),
+        sitemap({
+            changefreq: "always",
+            lastmod: new Date(),
+        }),
         mdx({ syntaxHighlight: "shiki" }),
+        svelte(),
     ],
 });
