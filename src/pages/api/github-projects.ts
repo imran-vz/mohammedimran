718f64425f4d6759931d85dd112e694a161141b2
Mohammed Imran <mohammedimran86992@gmail.com>
2023-11-19 22:50:48 +0530

    Add @vercel/kv package to package.json and pnpm-lock.yaml
    Update src/pages/api/github-projects.ts to use @vercel/kv for caching

--- src/pages/api/github-projects.ts
+++ src/pages/api/github-projects.ts
@@ -1,17 +1,38 @@
+import { createClient } from "@vercel/kv";
 import type { APIRoute } from "astro";
-import fetchTopLanguages from "../../utils/fetch-top-languages";
+import fetchTopLanguages, {
+    type TopLangData,
+} from "../../utils/fetch-top-languages";
 
 export const GET: APIRoute = async () => {
     try {
-        const response = await fetchTopLanguages("m0hammedimran", [
-            "angualar-todo-firebase",
-            "protoezy-graphy-mock",
-            "react-native-todo-app",
-            "headlessui-react-type-error-demo",
-            "wallpapers",
-            "Final-Year-Project",
-            "libsol",
-        ]);
+        const kv = createClient({
+            url: import.meta.env.KV_REST_API_URL,
+            token: import.meta.env.KV_REST_API_TOKEN,
+        });
+
+        let response: TopLangData | null = null;
+        const cacheResponse = await kv.hget<TopLangData>(
+            "github-projects",
+            "m0hammedimran",
+        );
+
+        if (!cacheResponse) {
+            response = await fetchTopLanguages("m0hammedimran", [
+                "angualar-todo-firebase",
+                "protoezy-graphy-mock",
+                "react-native-todo-app",
+                "headlessui-react-type-error-demo",
+                "wallpapers",
+                "Final-Year-Project",
+                "libsol",
+            ]);
+
+            await kv.hset("github-projects", { m0hammedimran: response });
+            await kv.expire("github-projects", 60 * 60 * 24 * 7);
+        }
+
+        response = response || cacheResponse;
         return new Response(JSON.stringify(response), {
             status: 200,
             headers: { "Content-Type": "application/json" },
