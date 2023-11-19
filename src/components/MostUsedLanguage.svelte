726e2343139d7d8e64823e98d8e502efa512bd6d
Mohammed Imran <mohammedimran86992@gmail.com>
2023-11-19 18:16:29 +0530

    Add noscript element for MostUsedLanguage component

--- src/components/MostUsedLanguage.svelte
+++ src/components/MostUsedLanguage.svelte
@@ -32,6 +32,26 @@
     <h4 class="text-xl font-bold mb-4">Most Used Languauge</h4>
 
     <div class="flex flex-wrap gap-4 max-w-6xl">
+        <noscript>
+            <ol class="flex flex-wrap gap-4 max-w-6xl overflow-x-scroll">
+                <li
+                    class="rounded-sm border border-pink-600/50 bg-zinc-900 bg-opacity-20 backdrop-blur-sm px-4 py-2 hover:bg-opacity-50 whitespace-nowrap"
+                >
+                    Javascript / TypeScript
+                </li>
+                <li
+                    class="rounded-sm border border-pink-600/50 bg-zinc-900 bg-opacity-20 backdrop-blur-sm px-4 py-2 hover:bg-opacity-50 whitespace-nowrap"
+                >
+                    Rust (Intermidiate)
+                </li>
+                <li
+                    class="rounded-sm border border-pink-600/50 bg-zinc-900 bg-opacity-20 backdrop-blur-sm px-4 py-2 hover:bg-opacity-50 whitespace-nowrap"
+                >
+                    Go lang
+                </li>
+            </ol>
+        </noscript>
+
         {#each Object.values(langs) as value}
             <div
                 class="flex gap-2 items-center px-4 py-2 border border-pink-600/50 rounded-sm snap-center"
