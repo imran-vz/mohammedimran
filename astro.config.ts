67fbad45a3445b23485b1f282a6122a1ef32008b
Mohammed Imran <mohammedimran86992@gmail.com>
2023-11-19 18:09:35 +0530

    Update dependencies and add Vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -2,16 +2,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-
+import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
     output: "hybrid",
-    adapter: node({
-        mode: "standalone",
-    }),
+    adapter: vercel(),
     site: "https://mohammedimran.com",
     integrations: [
         tailwind(),
