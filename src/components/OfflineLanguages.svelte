20c2a56db776126b0f9460ad53bef0c4e89645f3
Mohammed Imran <imran@factoreal.com>
2023-12-08 14:02:35 +0530

    Add OfflineLanguages component

---
+++ src/components/OfflineLanguages.svelte
@@ -0,0 +1,14 @@
+<ol class='flex flex-wrap gap-4 max-w-6xl overflow-x-auto'>
+	<li
+		class='rounded-sm border border-pink-600/50 bg-zinc-900 bg-opacity-20 backdrop-blur-sm px-4 py-2 hover:bg-opacity-50 whitespace-nowrap'>
+		Javascript / TypeScript
+	</li>
+	<li
+		class='rounded-sm border border-pink-600/50 bg-zinc-900 bg-opacity-20 backdrop-blur-sm px-4 py-2 hover:bg-opacity-50 whitespace-nowrap'>
+		Rust (Intermediate)
+	</li>
+	<li
+		class='rounded-sm border border-pink-600/50 bg-zinc-900 bg-opacity-20 backdrop-blur-sm px-4 py-2 hover:bg-opacity-50 whitespace-nowrap'>
+		Go lang
+	</li>
+</ol>
