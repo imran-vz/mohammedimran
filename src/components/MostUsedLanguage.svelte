883846cc3fa0af2ea06b898cac855b6d55036fdf
Mohammed Imran <imran@factoreal.com>
2023-12-08 14:02:29 +0530

    Refactor MostUsedLanguage component

--- src/components/MostUsedLanguage.svelte
+++ src/components/MostUsedLanguage.svelte
@@ -1,56 +1,45 @@
 <script lang="ts">
     import type { Lang } from "../utils/fetch-top-languages";
+    import OfflineLanguages from "./OfflineLanguages.svelte";
     export let totalLanguageSize: number = 0;
     export let langs: Lang[] = [];
 </script>
 
-<div class="px-6 py-4 bg-zinc-900 bg-opacity-80 rounded-sm">
+<div class="px-6 py-4 bg-skin-card bg-opacity-80 rounded-sm">
     <h2 class="text-xl font-bold mb-4">Most Used Language</h2>
 
     <div class="flex flex-wrap gap-4 max-w-6xl">
         <noscript>
-            <ol class="flex flex-wrap gap-4 max-w-6xl overflow-x-auto">
-                <li
-                    class="rounded-sm border border-pink-600/50 bg-zinc-900 bg-opacity-20 backdrop-blur-sm px-4 py-2 hover:bg-opacity-50 whitespace-nowrap"
-                >
-                    Javascript / TypeScript
-                </li>
-                <li
-                    class="rounded-sm border border-pink-600/50 bg-zinc-900 bg-opacity-20 backdrop-blur-sm px-4 py-2 hover:bg-opacity-50 whitespace-nowrap"
-                >
-                    Rust (Intermediate)
-                </li>
-                <li
-                    class="rounded-sm border border-pink-600/50 bg-zinc-900 bg-opacity-20 backdrop-blur-sm px-4 py-2 hover:bg-opacity-50 whitespace-nowrap"
-                >
-                    Go lang
-                </li>
-            </ol>
+            <OfflineLanguages />
         </noscript>
 
-        {#each Object.values(langs) as value}
-            <div
-                class="flex gap-2 items-center px-4 py-2 border border-pink-600/50 rounded-sm snap-center"
-            >
+        {#if langs.length === 0 && totalLanguageSize === 0}
+            <OfflineLanguages />
+        {:else}
+            {#each Object.values(langs) as value}
                 <div
-                    class="w-3 h-3 rounded-full"
-                    style="background-color: {value.color}"
-                />
-                <p class="whitespace-nowrap">
-                    {value.name} - {(
-                        (value.size / totalLanguageSize) *
-                        100
-                    ).toFixed(2)} %
-                </p>
-            </div>
-        {/each}
-        {#if langs.length}
+                    class="flex gap-2 items-center px-4 py-2 border border-skin-accent rounded-sm snap-center"
+                >
+                    <div
+                        class="w-3 h-3 rounded-full"
+                        style="background-color: {value.color}"
+                    />
+                    <p class="whitespace-nowrap text-skin-base">
+                        {value.name} - {(
+                            (value.size / totalLanguageSize) *
+                            100
+                        ).toFixed(2)} %
+                    </p>
+                </div>
+            {/each}
             <p class="text-xs">
                 🛈 These stats are pulled from <a
                     href="https://github.com/m0hammedimran"
+                    class="focus-outline focus-visible:outline-1"
                 >
-                    Github,
-                </a> and are based on the languages used in my public repositories.
+                    Github</a
+                >, and are based on the languages used in my public
+                repositories.
             </p>
         {/if}
     </div>
