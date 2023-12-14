82e17738683eda479f66a50be6c392ff3c3c7c6b
Mohammed Imran <imran@factoreal.com>
2023-12-14 14:47:18 +0530

    Refactor GitHub Projects API endpoint

--- src/pages/api/github-projects.ts
+++ src/pages/api/github-projects.ts
@@ -1,55 +1,84 @@
 import { createClient } from "@vercel/kv";
 import type { APIRoute } from "astro";
+import type { TopLanguages, TrimTopLanguagesResult } from "../../types";
 import fetchTopLanguages from "../../utils/fetch-top-languages";
 import { trimTopLanguages } from "../../utils/utils";
-import type { TopLanguages } from "../../types";
 
 const kv = createClient({
     url: import.meta.env.KV_REST_API_URL,
     token: import.meta.env.KV_REST_API_TOKEN,
 });
 
+const headers = new Headers();
+headers.set("Content-Type", "application/json");
+
 export const GET: APIRoute = async () => {
     try {
-        let response: TopLanguages | null = null;
-        const cacheResponse = await kv.hget<TopLanguages>(
-            "github-projects",
-            "m0hammedimran",
-        );
+        const trimTopLanguages = await getTrimTopLanguages();
 
-        if (!cacheResponse) {
-            response = await fetchTopLanguages("m0hammedimran", [
-                "angualar-todo-firebase",
-                "protoezy-graphy-mock",
-                "react-native-todo-app",
-                "headlessui-react-type-error-demo",
-                "wallpapers",
-                "Final-Year-Project",
-                "libsol",
-            ]);
-
-            await kv.hset("github-projects", { m0hammedimran: response });
-            await kv.expire("github-projects", 60 * 60 * 24 * 7);
-        }
-
-        response = response || cacheResponse;
-        const result = trimTopLanguages(response, 10, [
-            "Vim Script",
-            "AutoHotkey",
-            "Makefile",
-            "Shell",
-            "MDX",
-        ]);
-
-        return new Response(JSON.stringify(result), {
-            status: 200,
-            headers: { "Content-Type": "application/json" },
-        });
+        return new Response(JSON.stringify(trimTopLanguages), { headers });
     } catch (error) {
         console.log(error);
         return new Response(
             JSON.stringify({ message: "Internal Server Error" }),
-            { status: 500 },
+            { status: 500, headers },
         );
     }
 };
+
+async function getTopLanguages() {
+    let topLanguages: TopLanguages | null = null;
+    const cacheTopLanguagesResponse = await kv.hget<TopLanguages>(
+        "github-projects",
+        "m0hammedimran",
+    );
+
+    if (!cacheTopLanguagesResponse) {
+        topLanguages = await fetchTopLanguages("m0hammedimran", [
+            "angualar-todo-firebase",
+            "protoezy-graphy-mock",
+            "react-native-todo-app",
+            "headlessui-react-type-error-demo",
+            "wallpapers",
+            "Final-Year-Project",
+            "libsol",
+        ]);
+
+        await kv.hset("github-projects", { m0hammedimran: topLanguages });
+        await kv.expire("github-projects", 60 * 60 * 24 * 7);
+    }
+
+    topLanguages = topLanguages || cacheTopLanguagesResponse;
+    return topLanguages;
+}
+
+async function getTrimTopLanguages() {
+    let trimmedTopLanguages: TrimTopLanguagesResult | null = null;
+
+    let cacheResponse = await kv.hget<TrimTopLanguagesResult>(
+        "github-projects-parsed",
+        "m0hammedimran",
+    );
+
+    if (!cacheResponse) {
+        let topLanguages = await getTopLanguages();
+        trimmedTopLanguages = trimTopLanguages({
+            topLanguages,
+            languagesCount: 10,
+            hideLanguages: new Set([
+                "Vim Script",
+                "AutoHotkey",
+                "Makefile",
+                "Shell",
+            ]),
+        });
+        await kv.hset("github-projects-parsed", {
+            m0hammedimran: trimmedTopLanguages,
+        });
+        await kv.expire("github-projects-parsed", 60 * 60 * 24 * 7);
+    }
+
+    trimmedTopLanguages = trimmedTopLanguages || cacheResponse;
+
+    return trimmedTopLanguages;
+}
