acc0878076da083032fd6c1f7f603425725cc254
Mohammed Imran <mohammedimran86992@gmail.com>
2023-12-20 13:41:39 +0530

    Added tracking scripts

--- astro.config.ts
+++ astro.config.ts
@@ -1,23 +1,24 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import { defineConfig, squooshImageService } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig, squooshImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    image: {
-        service: squooshImageService(),
-    },
-    adapter: vercel({ webAnalytics: { enabled: true } }),
-    build: { inlineStylesheets: 'always' },
-    site: 'https://mohammedimran.com',
-    integrations: [
-        tailwind(),
-        sitemap({ changefreq: 'always', lastmod: new Date() }),
-        mdx({ syntaxHighlight: 'shiki' }),
-        svelte(),
-    ],
+	output: "server",
+	image: {
+		service: squooshImageService(),
+	},
+	adapter: vercel(),
+	build: { inlineStylesheets: "always" },
+	site: "https://mohammedimran.com",
+	integrations: [
+		tailwind(),
+		sitemap({ changefreq: "always", lastmod: new Date() }),
+		mdx({ syntaxHighlight: "shiki" }),
+		svelte(),
+	],
 });
+
