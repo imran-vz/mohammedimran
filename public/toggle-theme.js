4f6ce095d404df84b8943f647e1cd2402dce52b2
Mohammed Imran <imran@factoreal.com>
2023-12-29 18:48:11 +0530

    Refactor toggle-theme.js for improved readability and functionality

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -1,40 +1,52 @@
 const primaryColorScheme = "";
 const currentTheme = localStorage.getItem("theme");
 function getPreferTheme() {
-	return (
-		currentTheme || primaryColorScheme || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
-	);
+    return (
+        currentTheme ||
+        primaryColorScheme ||
+        (window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light")
+    );
 }
 let themeValue = getPreferTheme();
 function setPreference(themeValue) {
-	localStorage.setItem("theme", themeValue);
-	reflectPreference();
+    localStorage.setItem("theme", themeValue);
+    reflectPreference();
 }
 function reflectPreference() {
-	document.firstElementChild.setAttribute("data-theme", themeValue);
-	document.querySelector("#theme-btn")?.setAttribute("aria-label", themeValue);
-	const body = document.body;
-	if (body) {
-		const backgroundColor = window.getComputedStyle(body).backgroundColor;
-		document.querySelector("meta[name='theme-color']")?.setAttribute("content", backgroundColor);
-	}
+    document.firstElementChild.setAttribute("data-theme", themeValue);
+    document
+        .querySelector("#theme-btn")
+        ?.setAttribute("aria-label", themeValue);
+    const body = document.body;
+    if (body) {
+        const backgroundColor = window.getComputedStyle(body).backgroundColor;
+        document
+            .querySelector("meta[name='theme-color']")
+            ?.setAttribute("content", backgroundColor);
+    }
 }
 reflectPreference();
 
 window.onload = () => {
-	function swapTheme() {
-		reflectPreference();
-		document.querySelector("#theme-btn")?.addEventListener("click", () => {
-			themeValue = themeValue === "light" ? "dark" : "light";
-			setPreference(themeValue);
-		});
-	}
+    function swapTheme() {
+        reflectPreference();
+        document.querySelector("#theme-btn")?.addEventListener("click", () => {
+            themeValue = themeValue === "light" ? "dark" : "light";
+            setPreference(themeValue);
+            console.log("⚓️ | ftreal:", ftreal);
+            ftreal?.setUserId("mohammedimran@factoreal.com");
+        });
+    }
 
-	swapTheme();
-	document.addEventListener("astro:after-swap", swapTheme);
+    swapTheme();
+    document.addEventListener("astro:after-swap", swapTheme);
 };
 
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
-	themeValue = matches ? "dark" : "light";
-	setPreference(themeValue);
-});
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", ({ matches }) => {
+        themeValue = matches ? "dark" : "light";
+        setPreference(themeValue);
+    });
